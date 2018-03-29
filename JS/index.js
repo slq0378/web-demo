f0a7cbf851973a5f297a9af533821bb4b1f1eb7d
Mr. Song <slq0378@gmail.com>
2018-03-29 10:03:57 +0800

    Initial commit

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+
+function myFunction1()
+{
+    console.log('myFunction1');
+   // var x=document.getElementsByTagName('p')[0];  // 找到元素
+    // x.innerHTML="Hello JavaScript!";    // 改变内容
+
+}
+
+
+function myFunction2()
+{
+    x=document.getElementById("demo");  // 找到元素
+    x.innerHTML="Hello JavaScript!";    // 改变内容
+
+}
